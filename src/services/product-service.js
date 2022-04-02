c4ea39809b30499cf7b95ca4387c47fd105c4161
parthgabani777 <49564563+parthgabani777@users.noreply.github.com>
2022-04-02 13:52:44 +0530

    fix: api calls

--- src/services/product-service.js
+++ src/services/product-service.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const getProducts = async () => {
     try {
-        const { data } = await axios.get("/api/products1");
+        const { data } = await axios.get("/api/products");
         return data;
     } catch (error) {
         console.log(error);
